a98e8d21ad1863e4a3c7e42c4a93906d9625a73a
Moisés Rocha <moisesrocha99@gmail.com>
2019-11-26 23:04:51 +0000

    universal page rendering test on profile page

--- pages/profile.php
+++ pages/profile.php
@@ -1,40 +1,20 @@
 <?php 
 include_once('../config.php');
-include_once(ROOT . 'templates/common/header.php');
-include_once(ROOT.'templates/common/footer.php');
-include_once(ROOT .'templates/common/loginForm.php');
+include_once(ROOT . 'templates/drawTemplate.php');
+
+function draw_profile() { ?>
+  <div class="profile-content">
+    <div class="profile-picture-container">
+      <img src="/database/profilePictures/default" alt="Profile picture">
+    </div>
+    <div class="user-details">
+      <h1 class="name">Name</h1>
+      <p class="user-email">email@domain.com</p>
+    </div>
+  </div>
+<?php 
+} 
+
+renderPage(array('profile'), array(), draw_profile);
+
 ?>
-<html>
-  <head>
-    <title>Villat</title>
-    <meta charset="utf-8" />
-
-    <link rel="stylesheet" href="/stylesheets/common.css">
-    <link rel="stylesheet" href="/stylesheets/topbar.css">
-    <link rel="stylesheet" href="/stylesheets/login.css">
-    <link rel="stylesheet" href="/stylesheets/profile.css">
-
-    <script src="/javascript/login.js" defer></script>
-  </head>
-
-  <body>
-    <?php draw_header(); ?>
-
-    <section class="main-content">
-      <div class="profile-content">
-        <div class="profile-picture-container">
-          <img src="/database/profilePictures/default" alt="Profile picture">
-        </div>
-        <div class="user-details">
-          <h1 class="name">Name</h1>
-          <p class="user-email">email@domain.com</p>
-        </div>
-      </div>
-
-    </section>
-
-    <?php draw_login_form(); ?>
-
-    <?php draw_footer(); ?>
-  </body>
-</html>
