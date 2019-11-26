582b2a5408fff902684418e0d364bb2f20f4d67f
leonardofmoura <up201706907@fe.up.pt>
2019-11-26 19:42:05 +0000

    Changed index page to be generated from php templates

--- includes/db_houses.php
+++ includes/db_houses.php
@@ -2,7 +2,7 @@
 include_once('includes/database.php');
 
 function getHouseInfo() {
-    $db = Database::instance().db();
+    $db = Database::instance()->db();
 
     $stmt = $db->prepare('SELECT houseID,description FROM house');
     $stmt->execute();
--- templates/common/house_card.php
+++ templates/common/house_card.php
@@ -4,8 +4,6 @@ include_once('includes/db_houses.php');
 function draw_house_cards() {
   $houses = getHouseInfo();
 
-  echo $houses;
-
   foreach ($houses as $house) {
     draw_house($house);
   }
@@ -15,10 +13,10 @@ function draw_house_cards() {
 
 <?php function draw_house($house) { ?>
     <div class="house-card">
-    <div class="card-thumbnail"></div>
-    <div class="card-info">
-      <h3>House <?= $house['houseId'] ?> </h3>
-      <p><?= $house['description'] ?> </p>
-    </div>
+      <div class="card-thumbnail"></div>
+      <div class="card-info">
+        <h3>House <?= $house['houseID'] ?> </h3>
+        <p><?= $house['description'] ?> </p>
+      </div>
   </div>
 <?php } ?>
