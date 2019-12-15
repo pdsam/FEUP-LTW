9a7d21809868429cfaa98110703ac6ee93e46701
leonardofmoura <up201706907@fe.up.pt>
2019-12-15 13:34:11 +0000

    If a house has no reviews, that is said in the reviews tab

--- actions/action_getReviews.php
+++ actions/action_getReviews.php
@@ -5,16 +5,20 @@ include_once(ROOT . 'database/db_users.php');
 
 $reviews = getReviews($_GET['houseId']);
 
-foreach($reviews as $review) { 
-    $user = getUserById($review['userID']);?>
-
-    <article class="review">
-        <h1><?= $user['firstName']?>  <?=$user['lastName']?></h1>
-        <p><?= $review['reviewText'] ?></p>
-        <footer>
-            <p>Rating: <?= $review['rating'] ?></p>
-            <p>Date : <?= $review['postedDate'] ?></p>
-        </footer>
-    </article>
-
-<?php } ?>
+if (sizeof($reviews) === 0) { ?>
+    <p>This house has no reviews yet</p>
+<?php }
+else {
+    foreach($reviews as $review) { 
+        $user = getUserById($review['userID']);?>
+    
+        <article class="review">
+            <h1><?= $user['firstName']?>  <?=$user['lastName']?></h1>
+            <p><?= $review['reviewText'] ?></p>
+            <footer>
+                <p>Rating: <?= $review['rating'] ?></p>
+                <p>Date : <?= $review['postedDate'] ?></p>
+            </footer>
+        </article>
+    <?php } 
+} ?>
