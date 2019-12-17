c75be79bc07e1474de85fcde76192c565a25055e
pdsam <paulodsam@gmail.com>
2019-12-17 18:35:40 +0000

    started working on responsive navbar

--- pages/home.php
+++ pages/home.php
@@ -13,51 +13,49 @@ renderPage(array('home', 'forms'), array('slider'), function () {
   $nextDay = $nextDay->format('Y-m-d');
 
   $maxPrice = maxPrice()['max(pricePerNight)'];
-  
-  if(!empty($_GET)){
-    
 
-    $query = 'select * from house where 1';
+  if (!empty($_GET)) {
+
+    $query = 'SELECT *, location.name as address FROM house JOIN location using(locationID) where 1 ';
     $params = array();
 
-    if(array_key_exists('location',$_GET)&&$_GET['location'] != ""){
-        $query .= " and house.locationID=(select locationID from location where name=?) ";//prolly will break;
-        $params[] = $_GET['location'];
+    if (array_key_exists('location', $_GET) && $_GET['location'] != "") {
+      $query .= " and house.locationID=(select locationID from location where name=?) "; //prolly will break;
+      $params[] = $_GET['location'];
     }
 
-    if(array_key_exists('checkin',$_GET) && array_key_exists('checkout',$_GET)){
-        
-        
-        $query .= "and house.houseID not in (select houseID from reservation where julianDate(?)<julianDate(endDate) or julianDate(?) > julianDate(startDate))";
-        
-        $params[] = $_GET['checkin'];
-        $params[] = $_GET['checkout'];
+    if (array_key_exists('checkin', $_GET) && array_key_exists('checkout', $_GET) && $_GET['checkin']!="" && $_GET['checkout']) {
 
-    }
 
-    
-    
+      $query .= "and house.houseID not in (select houseID from reservation where julianDate(?)<julianDate(endDate) or julianDate(?) > julianDate(startDate))";
 
-    if(array_key_exists('capacity',$_GET)){
-        $query .= " and house.adress  like '%?%' ";//prolly will break;
-        $params[] = $_GET['capacity'];
-    }
-
-    if(array_key_exists('pricePerNight',$_GET)){
-        $query .= " and house.pricePerNight  < ? ";//prolly will break;
-        $params[] = $_GET['pricePerNight'];
+      $params[] = $_GET['checkin'];
+      $params[] = $_GET['checkout'];
     }
 
 
-   $houses= search($query,$params);
-  }
-  else{
-    
+
+
+    if (array_key_exists('capacity', $_GET)) {
+      $query .= " and house.capacity >= ? "; //prolly will break;
+      $params[] = $_GET['capacity'];
+    }
+
+    if (array_key_exists('priceRange', $_GET)) {
+      $query .= " and house.pricePerNight  <= ? "; //prolly will break;
+      $params[] = $_GET['priceRange'];
+    }
+
+    var_dump($query);
+    var_dump($params);
+    $houses = search($query, $params);
+  } else {
+
     $houses = getAllHouseInfo();
   }
 
 
-  ?>
+?>
   <section class="filter-renderer">
     <div class="search-form-container">
       <form class="filter-form" action="#" method="GET">
@@ -70,48 +68,49 @@ renderPage(array('home', 'forms'), array('slider'), function () {
             </div>
           </div>
 
-          <!--<div class="form-inline-container">-->
-            <div class="form-element">
-              <label class='block-label' for='start-date'>Check-In:</label>
-              <div>
-                <?= "<input type= 'date' id='start-date' min=$today >" ?>
-              </div>
-            </div>
 
-            <div class="form-element">
-              <label class='block-label' for='end-date'>Checkout:</label>
-              <div>
-                <?= "<input type= 'date' id='end-date' min=$nextDay >" ?>
-              </div>
+          <div class="form-element">
+            <label class="block-label" for="start-date">Check-In:</label>
+            <div>
+              <?= "<input type= 'date' id='start-date' min=$today name='checkin' >" ?>
             </div>
-          <!--</div>-->
+          </div>
+
+          <div class="form-element">
+            <label class="block-label" for="end-date">Checkout:</label>
+            <div>
+              <?= "<input type= 'date' id='end-date' min=$nextDay name='checkout'>" ?>
+            </div>
+          </div>
+
 
           <div class="form-element">
             <label class="block-label" for="numberPeople">Number of guests</label>
             <div>
-              <input type="number" id="numberPeople" value="1" min="1" max="100" step="1">
+              <input type="number" id="numberPeople" value="1" min="1" max="100" step="1" name="capacity">
             </div>
           </div>
           <!--<label class="block-label" for="priceRange">Price:</label>-->
           <div>
-            <p>Price: <span id="rangeValue"></span></p>
+            <p> Max Price: <span id="rangeValue"></span></p>
             <div>
-             <?= "<input type='range' name='priceRange' id='myRange' min='0' max='$maxPrice'>" ?>
+              <?= "<input type='range' name='priceRange' id='myRange' min='0' max='$maxPrice' value='$maxPrice'>" ?>
             </div>
           </div>
 
 
 
         </div>
-
-    </div>
-    <input class="submit-button" type="submit" value="Search">
-    </form>
+        
+        <div class="submit-button-wrapper">
+        <input class="submit-button" type="submit" value="Search">
+        </div> 
+      </form>
     </div>
   </section>
 
   <section class="houses-display">
     <?php
-    draw_house_cards($houses); ?>
+              draw_house_cards($houses); ?>
   </section>
 <?php }) ?>
--- templates/common/header.php
+++ templates/common/header.php
@@ -4,9 +4,9 @@ include_once(ROOT . 'database/db_users.php');
 
 
 function draw_header()
-{ 
+{
   $user = getSessionUser();
-  ?>
+?>
   <nav class="topbar">
     <section class="bar-logo">
       <a href="../index.php">
@@ -14,18 +14,17 @@ function draw_header()
       </a>
     </section>
     <?php if ($user) {
-      ?>
+    ?>
       <section class="user-options-container">
-        
+
         <?php
-        //$user = getUser($_SESSION['username']);
         $userID = $user['id'];
-       echo "<a href='../pages/profile.php?id=$userID'>" ;?>
-          <p class="nav-button">Profile</p>
+        echo "<a href='../pages/profile.php?id=$userID'>"; ?>
+        <p class="nav-button">Profile</p>
         </a>
         <a href="../pages/user_reservations.php">
-        <p class="nav-button">Your Reservations</p>
-      </a>
+          <p class="nav-button">Your Reservations</p>
+        </a>
         <?php if (isLandlord($user['id'])) { ?>
           <a href="../pages/dashboard.php">
             <p class="nav-button">Dashboard</p>
@@ -35,7 +34,7 @@ function draw_header()
           <p class="nav-button">Sign Out</p>
         </a>
       </section>
-    <?php } else { ?> 
+    <?php } else { ?>
       <section class="user-options-container">
         <p id="login-button" class="nav-button">Sign in</p>
 
@@ -43,6 +42,30 @@ function draw_header()
           <p id="register-button" class="nav-button">Sign up</p>
         </a>
       </section>
+      </a>
     <?php } ?>
+    <div class="menu">
+      <a href="javascript:void(0)" id="icon-menu" onClick="hamburguerFunction()">
+        <span></span>
+        <span></span>
+        <span></span>
+    </a>
+    </div>
+  </nav>
+  <nav class="hamburguer-nav">
+
+
+
+  <?php
+  if($user){ ?>
+
+<?php
+  }
+  else{ ?>
+      <div><p id="login-button" class="nav-button">Sign in</p></div>
+      <div><a href="../pages/register.php">
+          <p id="register-button" class="nav-button">Sign up</p>
+        </a></div>
+  <?php }?>
   </nav>
 <?php } ?>
