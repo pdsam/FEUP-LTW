719816aa16ef8239f2420abb045e9f90fbbdf0fa
Moisés Rocha <moisesrocha99@gmail.com>
2019-12-17 22:05:17 +0000

    fixed overflow on house cards

--- templates/common/house_card.php
+++ templates/common/house_card.php
@@ -16,12 +16,14 @@ function draw_house_cards($houses) {
 
 <a href="../pages/house.php?h=<?= $house['houseID'] ?>">
   <div class="house-card">
+    <div class="thumbnail-container">
     <?php if ($housePic === FALSE)  { ?>
       <div class="card-thumbnail" style="background-image: url('../house.jpg')"></div>
     <?php } 
     else { ?>
       <div class="card-thumbnail" style="background-image: url(../database/housePictures/<?= $housePic['pictureID']?>)"></div>
     <?php } ?>
+    </div>
     <div class="card-info">
       <h3 class="house-card-title clip-text"><?= $house['title'] ?></h3>
       <p class="house-card-address clip-text"><?= $house['address'] ?></p>
