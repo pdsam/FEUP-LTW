e17a583259bd6cb5ae2f0161c32be198385c901d
Moisés Rocha <moisesrocha99@gmail.com>
2019-12-18 05:14:42 +0000

    htaccess

--- includes/database.php
+++ includes/database.php
@@ -4,7 +4,7 @@ class Database {
     private $db = null;
 
     private function __construct() {
-        $this->db = new PDO('sqlite:' . ROOT . 'database/villat.db');
+        $this->db = new PDO('sqlite:' . ROOT . 'database/db/villat.db');
         $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->db->query('PRAGMA foreign_keys = ON');
